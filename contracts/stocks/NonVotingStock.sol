10b847aa92d150f292dfc9fb363133e3437437ed
Jorge Izquierdo <jorge@izqui.me>
2016-12-06 14:08:08 +0100

    Make non voting stock grantable

--- contracts/stocks/NonVotingStock.sol
+++ contracts/stocks/NonVotingStock.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.4.6;
 
 import "./Stock.sol";
 import "./IssueableStock.sol";
+import "./GrantableStock.sol";
 
-contract NonVotingStock is IssueableStock {
+contract NonVotingStock is IssueableStock, GrantableStock {
   uint8 public votesPerShare = 0;
 
   string public name = "Non-Voting Stock";
