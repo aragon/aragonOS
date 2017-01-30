77d97e56df53d78f7b604ba5d9b91159ecdb4cae
Jorge Izquierdo <jorge@izqui.me>
2017-01-30 13:43:02 +0100

    Deploy gas

--- migrations/2_deploy_company.js
+++ migrations/2_deploy_company.js
@@ -3,9 +3,9 @@ module.exports = (deployer) => {
   deployer.link(AccountingLib, Company)
   deployer.deploy(BylawsLib)
   deployer.link(BylawsLib, Company)
-  deployer.deploy(Company, { value: web3.toWei(1, 'ether') })
+  deployer.deploy(Company, { gas: 5e6, value: 1e18 })
     .then(() => deployer.deploy(VotingStock, Company.deployed().address))
     .then(() => deployer.deploy(NonVotingStock, Company.deployed().address))
-    .then(() => Company.deployed().addStock(VotingStock.deployed().address, 1000))
-    .then(() => Company.deployed().addStock(NonVotingStock.deployed().address, 10000))
+    .then(() => Company.deployed().addStock(VotingStock.deployed().address, 1e3))
+    .then(() => Company.deployed().addStock(NonVotingStock.deployed().address, 1e4))
 }
