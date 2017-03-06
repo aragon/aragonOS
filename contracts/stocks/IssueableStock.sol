d864225e70a071607fa749aec34f9489536f3abf
Jorge Izquierdo <jorge@izqui.me>
2017-03-06 18:13:53 +0100

    Fix

--- contracts/stocks/IssueableStock.sol
+++ contracts/stocks/IssueableStock.sol
@@ -5,7 +5,7 @@ import "./Stock.sol";
 contract IssueableStock is Stock {
   function issueStock(uint256 _value) onlyCompany {
     totalSupply = safeAdd(totalSupply, _value);
-    addShareholder(company);
+    if (shareholderIndex < 1) addShareholder(company);
     balances[company] = safeAdd(balances[company], _value);
   }
 
