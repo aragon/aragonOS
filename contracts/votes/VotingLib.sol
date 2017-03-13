cc092dc347cb76bf884a07247bbe6568d312602f
Jorge Izquierdo <jorge@izqui.me>
2017-03-13 11:14:46 +0100

    Keep pushing

--- contracts/votes/VotingLib.sol
+++ contracts/votes/VotingLib.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.8;
 
+import "zeppelin-solidity/contracts/token/ERC20.sol";
+
 library VotingLib {
   struct Voting {
     mapping (uint8 => uint256) optionVotes; // option -> totalVotes
@@ -44,8 +46,46 @@ library VotingLib {
     NewVoting(votingId, startTimestamp, closeTimestamp);
   }
 
+  function closeVoting(Votings storage self, uint256 votingId) {
+    int256 i = indexOf(self.openedVotings, votingId);
+    if (i < 0) throw;
+
+    // Remove from array without keeping its order
+    if (self.openedVotings.length > 1) {
+      // Move last element to the place of the removing item
+      self.openedVotings[uint256(i)] = self.openedVotings[self.openedVotings.length - 1];
+    }
+    // Remove last item
+    self.openedVotings.length -= 1;
+  }
+
+  function canVote(Votings storage self, address voter, uint256 votingId) constant returns (bool) {
+    Voting voting = self.votings[votingId];
+    if (now > voting.closeTimestamp) return false; // poll is closed by date
+    if (voting.executed > 0) return false; // poll has been executed
+    if (voter == address(this)) return false; // non assigned stock cannot vote
+
+    for (uint j = 0; j < voting.governanceTokens.length; j++) {
+      address token = voting.governanceTokens[j];
+      if (ERC20(token).balanceOf(voter) > voting.voters[msg.sender][token]) return true; // can vote using token
+    }
+
+    return false;
+  }
+
+  function indexOf(uint256[] array, uint256 element) returns (int256) {
+    for (uint256 i = 0; i < array.length; i++) {
+      if (array[i] == element) return int256(i);
+    }
+    return -1;
+  }
+
+  function castVote(Votings storage self, uint256 votingId, uint8 vote) {
+
+  }
+
   function countVotes();
-  function castVote();
+
 
   event NewVoting(uint256 id, uint64 starts, uint64 closes);
   event VoteCasted(uint256 id, address voter, uint256 votes);
