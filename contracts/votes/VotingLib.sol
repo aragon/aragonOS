a8fec4f6f7c248b4af8cd4f68f24c1d7cf3af980
Jorge Izquierdo <jorge@izqui.me>
2017-03-16 19:36:08 +0100

    Finish developing voting liv

--- contracts/votes/VotingLib.sol
+++ contracts/votes/VotingLib.sol
@@ -5,10 +5,10 @@ import "../stocks/GovernanceToken.sol";
 
 library VotingLib {
   struct Voting {
-    mapping (uint8 => uint256) optionVotes; // option -> totalVotes
-    mapping (address => uint8) votedOption; // voter -> voted option
-    mapping (address => mapping (address => uint256)) voters; // voter -> governance token -> votes
-    mapping (address => uint256) overruledVotes;
+    mapping (uint8 => uint256) optionVotes; // option -> totalVotes (absolute votes)
+    mapping (address => uint8) votedOption; // voter -> voted option (absolute votes)
+    mapping (address => mapping (address => uint256)) voters; // voter -> governance token -> tokens voted
+    mapping (address => mapping (address => uint256)) overruledVotes; // delegate -> governance token -> overruledVotes (absolute votes)
     address[] governanceTokens;
     uint8[] tokenWeights;
     uint256 totalCastedVotes;
@@ -50,6 +50,8 @@ library VotingLib {
   }
 
   function closeVoting(Votings storage self, uint256 votingId) {
+    Voting voting = self.votings[votingId];
+    if (voting.executed == 0 && now < voting.closeTimestamp) throw; // Not executed nor closed by time
     int256 i = indexOf(self.openedVotings, votingId);
     if (i < 0) throw;
 
@@ -62,12 +64,17 @@ library VotingLib {
     self.openedVotings.length -= 1;
   }
 
-  function canVote(Votings storage self, address voter, uint256 votingId) constant returns (bool) {
+  function canModifyVote(Votings storage self, address voter, uint256 votingId) constant returns (bool) {
     Voting voting = self.votings[votingId];
     if (now > voting.closeTimestamp) return false; // poll is closed by date
     if (voting.executed > 0) return false; // poll has been executed
     if (voter == address(this)) return false; // non assigned stock cannot vote
+  }
 
+  function canVote(Votings storage self, address voter, uint256 votingId) constant returns (bool) {
+    if (!canModifyVote(self, voter, votingId)) return false;
+
+    Voting voting = self.votings[votingId];
     for (uint j = 0; j < voting.governanceTokens.length; j++) {
       address token = voting.governanceTokens[j];
       if (GovernanceToken(token).votingPowerForDelegate(voter) > voting.voters[msg.sender][token]) return true; // can vote using token
@@ -83,24 +90,30 @@ library VotingLib {
     return -1;
   }
 
-  function castVote(Votings storage self, uint256 votingId, uint8 vote) {
+  function castVote(Votings storage self, uint256 votingId, uint8 vote) returns (bool voted) {
     if (!canVote(self, msg.sender, votingId)) throw;
 
     Voting voting = self.votings[votingId];
     for (uint j = 0; j < voting.governanceTokens.length; j++) {
       GovernanceToken token = GovernanceToken(voting.governanceTokens[j]);
-      uint remainingVotes = token.votingPowerForDelegate(msg.sender) - voting.voters[msg.sender][token] - voting.overruledVotes[msg.sender];
+      uint remainingVotes = token.votingPowerForDelegate(msg.sender) - voting.voters[msg.sender][token] - voting.overruledVotes[msg.sender][token];
       uint addingVotes = token.votingPower() * remainingVotes;
 
       voting.voters[msg.sender][token] += remainingVotes;
       voting.optionVotes[vote] += addingVotes;
       voting.totalCastedVotes += addingVotes;
-      if (voting.votedOption[msg.sender] != 0 && voting.votedOption[msg.sender] != 10 + vote) throw; // cant vote different thingys
+      if (voting.votedOption[msg.sender] != 0 && voting.votedOption[msg.sender] != 10 + vote) throw; // cant vote different things
       voting.votedOption[msg.sender] = 10 + vote; // avoid 0
+
+      if (addingVotes > 0) voted = true;
     }
+
+    if (voted) VoteCasted(votingId, msg.sender);
   }
 
   function modifyVote(Votings storage self, uint256 votingId, uint8 vote, bool removes) {
+    if (!canModifyVote(self, msg.sender, votingId)) throw;
+
     Voting voting = self.votings[votingId];
     for (uint j = 0; j < voting.governanceTokens.length; j++) {
       GovernanceToken token = GovernanceToken(voting.governanceTokens[j]);
@@ -112,8 +125,11 @@ library VotingLib {
 
       if (token.votingPowerForDelegate(msg.sender) == 0 && remainingVotes > 0) {
         // over-write delegate vote
+
+        if (voting.votedOption[msg.sender] == 1) throw; // already overruled
         voting.optionVotes[oldOption] -= remainingVotes * token.votingPower();
-        voting.overruledVotes[token.votingDelegate(msg.sender)] = remainingVotes;
+        voting.overruledVotes[token.votingDelegate(msg.sender)][token] += remainingVotes;
+
         if (removes) {
           voting.votedOption[msg.sender] = 1; // overruled by removing
           voting.voters[msg.sender][token] = 0;
@@ -124,7 +140,7 @@ library VotingLib {
           voting.optionVotes[vote] += remainingVotes * token.votingPower();
         }
       } else {
-        uint totalVotes = voting.voters[msg.sender][token] - voting.overruledVotes[msg.sender];
+        uint totalVotes = voting.voters[msg.sender][token] - voting.overruledVotes[msg.sender][token];
         voting.optionVotes[oldOption] -= totalVotes * token.votingPower();
         uint modifyingVotes = totalVotes * token.votingPower();
         if (removes) {
@@ -140,10 +156,35 @@ library VotingLib {
     }
   }
 
-  function hasVoted();
-  function countVotes();
+  function hasVoted(Votings storage self, uint256 votingId, address voter) returns (bool) {
+    Voting voting = self.votings[votingId];
+    for (uint j = 0; j < voting.governanceTokens.length; j++) {
+      GovernanceToken token = GovernanceToken(voting.governanceTokens[j]);
+      if (voting.votedOption[token.votingDelegate(voter)] > 0) {
+        if (voting.votedOption[msg.sender] != 1) return true; // not overruled
+      }
+    }
+    return false;
+  }
 
+  function countVotes(Votings storage self, uint256 votingId, uint8 option) returns (uint256 votes, uint256 totalCastedVotes, uint256 votingPower) {
+    Voting voting = self.votings[votingId];
 
-  event NewVoting(uint256 id, uint64 starts, uint64 closes);
-  event VoteCasted(uint256 id, address voter, uint256 votes);
+    totalCastedVotes = voting.totalCastedVotes;
+    for (uint j = 0; j < voting.governanceTokens.length; j++) {
+      votes += voting.optionVotes[option];
+      GovernanceToken token = GovernanceToken(voting.governanceTokens[j]);
+      votingPower += (token.totalSupply() - token.balanceOf(this)) * token.votingPower();
+    }
+  }
+
+  // Company knows when it can be closed, nothing to be checked here
+  function closeExecutedVoting(Votings storage self, uint256 votingId, uint8 option) {
+    Voting voting = self.votings[votingId];
+    voting.executed = option;
+    closeVoting(self, votingId);
+  }
+
+  event NewVoting(uint256 indexed id, uint64 starts, uint64 closes);
+  event VoteCasted(uint256 indexed id, address indexed voter);
 }
