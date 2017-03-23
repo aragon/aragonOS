7f641a808994f9a75f53bf4e82794c2490d08198
Jorge Izquierdo <jorge@izqui.me>
2017-03-23 17:15:06 +0100

    Implement missing protocols

--- contracts/stocks/Stock.sol
+++ contracts/stocks/Stock.sol
@@ -6,6 +6,9 @@ import "./GovernanceToken.sol";
 
 // transferrableTokens is called from governance to vested
 contract Stock is VestedToken, GovernanceToken {
+  uint public decimals = 0;
+  address public parentToken = 0x0;
+
   function transferableTokens(address holder, uint64 time) constant public returns (uint256) {
     return min256(VestedToken.transferableTokens(holder, time), GovernanceToken.transferableTokens(holder, time));
   }
