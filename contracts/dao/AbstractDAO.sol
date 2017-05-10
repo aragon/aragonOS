adff3144db19f9308635077dcbd604265a0d0755
Jorge Izquierdo <jorge@izqui.me>
2017-05-10 12:54:12 +0200

    Document AbstractDAO.sol

--- contracts/dao/AbstractDAO.sol
+++ contracts/dao/AbstractDAO.sol
@@ -1,15 +1,10 @@
 pragma solidity ^0.4.11;
 
+// @dev AbstractDAO defines the storage the DAO has.
+// Contracts that will be delegate-called from the DAO need to inherit from
+// AbstractDAO to ensure memory is correctly off-setted
+
 contract AbstractDAO {
   address public self;
   address public kernel;
 }
-
-/*
-  function replaceKernel(address newKernel);
-  function getOrgan(uint organN) returns (address organAddress);
-  function replaceOrgan(address organAddress, uint organN);
-
-  event OrganReplaced(address organAddress, uint organN);
-}
-*/
