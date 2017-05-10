94e42b2dd49416e6c7e96ef25ff25e555e7c5372
Jorge Izquierdo <jorge@izqui.me>
2017-05-10 12:52:26 +0200

    Document DAO.sol and replace if/throw for assert

--- contracts/dao/DAO.sol
+++ contracts/dao/DAO.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.4.11;
 import "./AbstractDAO.sol";
 import "../kernel/DAOKernel.sol";
 
+// @dev DAO is the base contract on top of which all DAO lives.
+// This is the only element of the DAO that is non-upgradeable
+// Given the simplicity of this contract, it could be written in LLL and/or
+// be formally proven.
+
 contract DAO is AbstractDAO {
+  // @dev DAO constructor deploys its DAO kernel and saves its own identity as self
   function DAO() {
     kernel = address(new DAOKernel());
     self = address(this);
   }
 
-  function () payable {
-    if (!kernel.delegatecall(msg.data)) throw;
+  // @dev All calls to the DAO are forwarded to the kernel with a delegatecall
+  function () payable public {
+    assert(kernel.delegatecall(msg.data)); // In case the call fails, revert state.
   }
 }
