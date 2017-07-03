5c54cf84a5cc6cb0fb277f8e8d147c1a0109e6c2
Jorge Izquierdo <jorge@izqui.me>
2017-07-03 12:53:40 +0200

    Keep track of nonce to allow parallel deployment

--- migrations/2_deploy_dao.js
+++ migrations/2_deploy_dao.js
@@ -7,6 +7,18 @@ var VaultOrgan = artifacts.require('VaultOrgan')
 
 const organs = [VaultOrgan, TokensOrgan, ActionsOrgan, ApplicationOrgan]
 
+const getNonce = () => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getAccounts((err, acc) => {
+      if (err) return reject(err)
+      web3.eth.getTransactionCount(acc[0], (err, n) => {
+        if (err) return reject(err)
+        resolve(n)
+      })
+    })
+  })
+}
+
 module.exports = (deployer) => {
   let dao = {}
 
@@ -15,15 +27,23 @@ module.exports = (deployer) => {
       dao = MetaOrgan.at(DAO.address)
 
       const firstOrgan = 3
+      let currentNonce = 0
 
       const installOrgan = (organ, i) => {
         const n = i + firstOrgan
         console.log('Installing', organ.constructor._json.contract_name, organ.address, 'at slot', n)
-        return dao.installOrgan(organ.address, n)
+        currentNonce += 1
+
+        return dao.installOrgan(organ.address, n, { nonce: currentNonce + i + 1 })
       }
 
-      const installOrgans = Promise.all(organs.map(organ => organ.new()))
-                              .then(x => Promise.all(x.map(installOrgan)))
-      return installOrgans
+      return getNonce().then(x => {
+        const installOrgans = Promise.all(organs.map((organ, i) => {
+          currentNonce = x + i
+          return organ.new({ nonce: currentNonce })
+        }))
+          .then(x => Promise.all(x.map(installOrgan)))
+        return installOrgans
+      })
     })
 }
--- migrations/3_install_apps.js
+++ migrations/3_install_apps.js
@@ -4,10 +4,23 @@ var MetaOrgan = artifacts.require('MetaOrgan')
 
 const appNames = ['BylawsApp', 'OwnershipApp', 'VotingApp', 'StatusApp']
 
-const deployApps = daoAddress => {
+let nonce = 0
+const getNonce = () => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getAccounts((err, acc) => {
+      if (err) return reject(err)
+      web3.eth.getTransactionCount(acc[0], (err, n) => {
+        if (err) return reject(err)
+        resolve(n)
+      })
+    })
+  })
+}
+
+const deployApps = (daoAddress, non) => {
   const appsDeploy = appNames
                       .map(n => artifacts.require(n))
-                      .map(appContract => appContract.new(daoAddress))
+                      .map((appContract, i) => appContract.new(daoAddress, { nonce: non + i }))
 
   return Promise.all(appsDeploy)
 }
@@ -16,10 +29,10 @@ module.exports = (deployer) => {
   let dao, dao_apps = {}
   let bylawsAddress = ''
 
-  const installApp = (app, i) => {
+  const installApp = (app, i, non) => {
     const n = i + 1
     console.log('Installing app', app.constructor._json.contract_name, app.address, 'at slot', n)
-    return dao_apps.installApp(n, app.address)
+    return dao_apps.installApp(n, app.address, { nonce: non + i })
   }
 
   return deployer
@@ -27,12 +40,15 @@ module.exports = (deployer) => {
     .then(d => {
       dao = d
       dao_apps = ApplicationOrgan.at(dao.address)
-
-      return deployApps(dao.address)
+      return getNonce()
+    })
+    .then(x => {
+      nonce = x
+      return deployApps(dao.address, x)
     })
     .then(deployedApps => {
       bylawsAddress = deployedApps[0].address
-      return Promise.all(deployedApps.map(installApp))
+      return Promise.all(deployedApps.map((x, i) => installApp(x, i, appNames.length + nonce)))
     })
     .then(() => {
       console.log('Setting BylawsApp as DAO permissions oracle')
