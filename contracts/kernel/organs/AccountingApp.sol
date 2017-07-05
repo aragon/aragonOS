4a883257ff3b076a4dfd5b3d849dfc819710e5a5
Tim Watts <tim@readevalprint.com>
2017-07-05 12:56:18 +0200

    Formatting comment

--- contracts/kernel/organs/AccountingApp.sol
+++ contracts/kernel/organs/AccountingApp.sol
@@ -38,36 +38,59 @@ contract AccountingApp {
         uint timestamp; 
     }    
     
-    // Audit trail of Actions that were performed on a Transation
+    // Audit trail of Actions that were performed on a Transaction
     // For example ContractBob can initiate a transation to pay Eve for her invoice to Alice
+    /*
     
-    // 1) Make the new transation
-    //  tid = Transation(value:3, token:Foo, from:Alice, to: Eve, reference: "invoice 123")
-    // 
+     1) Make the new transation
+    tid = Transation(value:3, 
+                         token:Foo, 
+                         from:Alice, 
+                         to: Eve, 
+                         reference: "invoice 123")
     
-    // 2) Add the reason for the transation new state
-    // aid = TransactionAction(transactionId: tid, state: New, reason:"new invoice", actor: ContractBob)
-    // tid.transactionStates.push(aid)
+     2) Add the reason for the transation new state
+     aid = TransactionAction(transactionId: tid, 
+                             state: New, 
+                             reason:"new invoice", 
+                             actor: ContractBob)
+     tid.transactionStates.push(aid)
     
-    // 3) Add the reason for the transation new state
-    // aid = TransactionAction(transactionId: tid, state: Approved, reason:"verified the invoice", actor: AdminSam)
-    // tid.transactionStates.push(aid)
+     3) Add the reason for the transation new state
+     aid = TransactionAction(transactionId: tid, 
+                             state: Approved, 
+                             reason:"verified the invoice", 
+                             actor: AdminSam)
+     tid.transactionStates.push(aid)
     
-    // 3) Alice attemtps to with draw and now it's in the state where someone has a last chance to cancel it.
-    // aid = TransactionAction(transactionId: tid, state: Pending, reason:"withdawing", actor: Alice)
-    // tid.transactionStates.push(aid)
+     3) Alice attemtps to with draw and now it is in the state where someone has a last chance to cancel it.
+     aid = TransactionAction(transactionId: tid, 
+                             state: Pending, 
+                             reason:"withdawing", 
+                             actor: Alice)
+     tid.transactionStates.push(aid)
     
-    // 5.a) Yay, Eve's address accepts this token!
-    // aid = TransactionAction(transactionId: tid, state: Succeeded, reason:"", VaultOrgan)
-    // tid.transactionStates.push(aid)
+     5.a) Yay, Eves address accepts this token!
+     aid = TransactionAction(transactionId: tid, 
+                             state: Succeeded, 
+                             reason:"", 
+                             actor: VaultOrgan)
+     tid.transactionStates.push(aid)
     
-    // 5.b) Oh no, Eve's address doesn't accept this token!
-    // aid = TransactionAction(transactionId: tid, state: Failed, reason:"token rejected", actor: VaultOrgan)
-    // tid.transactionStates.push(aid)
+     5.b) Oh no, Eve's address doesn't accept this token!
+     aid = TransactionAction(transactionId: tid, 
+                             state: Failed, 
+                             reason:"token rejected", 
+                             actor: VaultOrgan)
+     tid.transactionStates.push(aid)
     
-    // 5.c) Wait, and admin canceled the transaction at the last moment
-    // aid = TransactionAction(transactionId: tid, state: Canceled, reason:"This is a fake invoice", actor: SomeOtherAdmin)
-    // tid.transactionStates.push(aid)
+     5.c) Wait, and admin canceled the transaction at the last moment
+     aid = TransactionAction(transactionId: tid, 
+                             state: Canceled, 
+                             reason:"This is a fake invoice", 
+                             actor: SomeOtherAdmin)
+     tid.transactionStates.push(aid)
+    */
 
     function AccountingApp() {
         
