6ea12946f354778c99b41ab023579d45097a7ad1
Jorge Izquierdo <jorge@izqui.me>
2017-07-06 21:17:43 +0200

    Fix isHolder

--- contracts/apps/ownership/OwnershipApp.sol
+++ contracts/apps/ownership/OwnershipApp.sol
@@ -124,8 +124,8 @@ contract OwnershipApp is OwnershipConstants, Application, Controller, Requestor 
   }
 
   function isHolder(address _holder) constant returns (bool) {
-    uint tokenCount = tokens.length;
-    for (uint i = 0; i < tokenCount; i++) {
+    uint tokenCount = getTokenCount();
+    for (uint i = 1; i <= tokenCount; i++) {
       address tknAddr = getTokenAddress(i);
       if (ERC20(tknAddr).balanceOf(_holder) >= holderThreshold) return true;
     }
