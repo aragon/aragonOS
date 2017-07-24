f65c174dda8cb2e4425dfac22b9c15249c1f59f7
Tim Watts <tim@readevalprint.com>
2017-07-24 17:00:41 +0200

    rm un used test

--- test/integration_accounting.js
+++ test/integration_accounting.js
@@ -55,7 +55,6 @@ contract('AccountingApp', accounts => {
     it('can update transaction', async () => {
         await dao_accountingApp.setDefaultAccountingPeriodSettings('0x111', '0', '*', '*', '0', '*'); // 5  new accounting period every sunday at midnight
         await dao_accountingApp.startNextAccountingPeriod()
-        await dao_accountingApp.startNextAccountingPeriod()
         await dao_accountingApp.newTransaction( '0x111', '0x100', 100, 'Ref 123')
         await dao_accountingApp.updateTransaction(0, 1, 'needs approval')
         let ti0 = await dao_accountingApp.getTransactionInfo.call(0)
@@ -65,19 +64,6 @@ contract('AccountingApp', accounts => {
         assert.equal(ts0[1], 'needs approval', 'should have a "reason" of "need approval"')
     })
 
-    it('can create new accounting periods', async () => {
-        ///await accountingApp.setDefaultAccountingPeriodSettings('0x111', '0', '*', '*', '0'); // 5  new accounting period every sunday at midnight
-        //await accountingApp.startNextAccountingPeriod()
-        //await accountingApp.newTransaction( '0x111', 100, '0x100', 'Ref 123', 0)
-        //let t0 = await accountingApp.getTransactionState(0)
-        //assert.equal(t0[5], 0, 'Should be in the 0th accountingPeriod')
-
-        //await accountingApp.startNextAccountingPeriod()
-        //await accountingApp.newTransaction( '0x111', 100, '0x100', 'Ref 345', 0)
-        //let t1 = await accountingApp.getTransactionState(1)
-        //assert.equal(t1[2], 'Ref 345', 'Should have matching reference number')
-        //assert.equal(t1[5], 1, 'Should be in the 1st accountingPeriod')
-    })
   })
 
 })
