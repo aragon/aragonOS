e9bb7f6fe8e93aa6cd4b1e54c820c4507a9e7c90
Jorge Izquierdo <jorge@izqui.me>
2017-09-04 11:46:52 +0200

    Clean up

--- contracts/dao/DAOEvents.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.11;
-
-import "../organs/IVaultOrgan.sol";
-import "../organs/IMetaOrgan.sol";
-import "../kernel/IKernelRegistry.sol";
-
-contract DAOEvents is IKernelRegistryEvents, IMetaOrganEvents, IVaultOrganEvents {}
--- contracts/dao/IDAO.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.11;
-
-
-// @dev IDAO defines the storage the DAO has.
-contract IDAO {
-    function getSelf() constant public returns (address);
-    function getKernel() constant public returns (address);
-}
