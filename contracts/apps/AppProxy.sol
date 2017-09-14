6e9cbc5bf90edde2ac713ef7c109d217b446e730
Jorge Izquierdo <jorge@izqui.me>
2017-09-14 19:33:33 +0200

    Abstract DelegateProxy logic. Use EIP211 new opcodes for allowing dynamically sized returns

--- contracts/apps/AppProxy.sol
+++ contracts/apps/AppProxy.sol
@@ -1,22 +1,17 @@
 pragma solidity 0.4.15;
 
 import "./AppStorage.sol";
+import "../common/DelegateProxy.sol";
 
-contract AppProxy is AppStorage {
+contract AppProxy is AppStorage, DelegateProxy {
     function AppProxy(Kernel _kernel, bytes32 _appId) {
         kernel = _kernel;
         appId = _appId;
     }
 
     function () payable public {
-        uint32 len = 320; // 10 return size
         address target = kernel.getAppCode(appId);
         require(target > 0); // if app code hasn't been set yet, don't call
-        assembly {
-            calldatacopy(0x0, 0x0, calldatasize)
-            let result := delegatecall(sub(gas, 10000), target, 0x0, calldatasize, 0, len)
-            switch result case 0 { invalid() }
-            return (0, len)
-        }
+        delegatedFwd(target, msg.data);
     }
 }
---
+++ contracts/common/DelegateProxy.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.4.15;
+
+contract DelegateProxy {
+    /**
+    * @dev Performs a delegatecall and returns whatever the delegatecall returned (entire context execution will return!)
+    * @param _dst Destination address to perform the delegatecall
+    * @param _calldata Calldata for the delegatecall
+    */
+    function delegatedFwd(address _dst, bytes _calldata) internal {
+        assembly {
+            switch extcodesize(_dst) case 0 { revert(0, 0) }
+            let result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
+            switch result case 0 { invalid() }
+            let size := returndatasize
+            switch size case 0 { return(0, 0) }
+
+            let ptr := mload(0x40)
+            returndatacopy(ptr, 0, size)
+            return(ptr, size)
+        }
+    }
+}
