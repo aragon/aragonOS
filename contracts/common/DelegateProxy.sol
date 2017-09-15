36ff6fa10d1d83dc7062a437d666a31614040e5f
Jorge Izquierdo <izqui97@gmail.com>
2017-09-15 12:52:42 +0200

    Revert instead of invalid on call result

--- contracts/common/DelegateProxy.sol
+++ contracts/common/DelegateProxy.sol
@@ -10,7 +10,7 @@ contract DelegateProxy {
         assembly {
             switch extcodesize(_dst) case 0 { revert(0, 0) }
             let result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
-            switch result case 0 { invalid() }
+            switch result case 0 { revert(0, 0 } // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
             let size := returndatasize
             switch size case 0 { return(0, 0) }
 
