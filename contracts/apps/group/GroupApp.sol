5a6ceb57be444ca7cd61245c7ebe53d0276b1936
Jorge Izquierdo <jorge@izqui.me>
2017-09-15 19:15:55 +0200

    Add identity in group app

--- contracts/apps/group/GroupApp.sol
+++ contracts/apps/group/GroupApp.sol
@@ -6,9 +6,17 @@ import "../../common/IForwarder.sol";
 import "../../common/EVMCallScript.sol";
 
 contract GroupApp is App, Initializable, IForwarder, EVMCallScriptRunner {
-    mapping (address => bool) member;
+    struct Identity {
+        bool isMember;
+        string name;
+        bytes contentURI;
+    }
+
+    mapping (address => Identity) public members;
+    bool public isWhitelist;
 
     event AddMember(address indexed addr);
+    event UpdateIdentity(address indexed addr, string name);
     event RemoveMember(address indexed addr);
 
     modifier onlyMember {
@@ -16,22 +24,34 @@ contract GroupApp is App, Initializable, IForwarder, EVMCallScriptRunner {
         _;
     }
 
-    function initialize(string _ctx) {
+    function initialize(string _ctx, bool _isWhitelist) onlyInit {
         initialized();
 
         context = _ctx;
+        isWhitelist = _isWhitelist;
     }
 
-    function addMember(address _addr) auth external {
+    function addMember(address _addr, string _name, bytes _contentURI) auth external {
         require(!isMember(_addr));
-        member[_addr] = true;
+        members[_addr].isMember = true;
+        _setIdentity(_addr, _name, _contentURI);
         AddMember(_addr);
     }
 
     function removeMember(address _addr) auth external {
         require(isMember(_addr));
-        member[_addr] = false;
+        members[_addr].isMember = false;
         RemoveMember(_addr);
+    }
+
+    function setIdentity(address _addr, string _name, bytes _contentURI) auth external {
+        _setIdentity(_addr, _name, _contentURI);
+    }
+
+    function _setIdentity(address _addr, string _name, bytes _contentURI) internal {
+        members[_addr].name = _name;
+        members[_addr].contentURI = _contentURI;
+        UpdateIdentity(_addr, _name);
     }
 
     function forward(bytes _evmCallScript) onlyMember external {
@@ -39,11 +59,11 @@ contract GroupApp is App, Initializable, IForwarder, EVMCallScriptRunner {
     }
 
     function isMember(address _addr) constant returns (bool) {
-        return member[_addr];
+        return members[_addr].isMember;
     }
 
     function canForward(address _sender, bytes _evmCallScript) constant returns (bool) {
         _evmCallScript;
-        return isMember(_sender);
+        return isWhitelist == isMember(_sender);
     }
 }
