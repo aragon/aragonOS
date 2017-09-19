868d3cbc91e43b8742055f3da6b7b3799ce283c4
Jorge Izquierdo <jorge@izqui.me>
2017-09-19 13:07:56 +0200

    Add roles to Kernel and App

--- contracts/apps/App.sol
+++ contracts/apps/App.sol
@@ -3,17 +3,12 @@ pragma solidity 0.4.15;
 import "./AppStorage.sol";
 
 contract App is AppStorage {
-    modifier auth {
-        require(canPerform(msg.sender, msg.sig));
+    modifier auth(bytes32 _role) {
+        require(canPerform(msg.sender, _role));
         _;
     }
 
-    modifier authSig(bytes4 sig) {
-        require(canPerform(msg.sender, sig));
-        _;
-    }
-
-    function canPerform(address _sender, bytes4 _sig) returns (bool) {
-        return address(kernel) == 0 || kernel.canPerform(_sender, address(this), _sig);
+    function canPerform(address _sender, bytes32 _role) returns (bool) {
+        return address(kernel) == 0 || kernel.canPerform(_sender, address(this), _role);
     }
 }
