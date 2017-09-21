799b3d51f4ca24733c5459429f0984a8bf984eef
Oliver <onbjerg@users.noreply.github.com>
2017-09-21 15:43:26 +0200

    Expose artifacts and contracts

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var glob = require('glob')
 var path = require('path')
-var basePath = './artifacts'
 
-module.exports = glob.sync(
-  path.resolve(__dirname, basePath, '*.json')
+var artifactsBasePath = './artifacts'
+var contractsBasePath = './build/contracts'
+
+module.exports.artifacts = glob.sync(
+  path.resolve(__dirname, artifactsBasePath, '*.json')
+).reduce(function (artifacts, file) {
+  var artifact = require(file)
+
+  if (artifact.id) {
+    artifacts[artifact.id] = artifact
+  }
+
+  return artifacts
+}, {})
+
+module.exports.contracts = glob.sync(
+  path.resolve(__dirname, contractsBasePath, '*.json')
 ).reduce(function (contracts, file) {
   var contract = require(file)
 
