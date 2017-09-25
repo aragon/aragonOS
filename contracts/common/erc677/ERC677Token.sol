9cd2abceb09aaf829c2816d6996997766d42d21c
Jorge Izquierdo <jorge@aragon.one>
2017-09-25 20:39:29 +0200

    Implement EtherToken

---
+++ contracts/common/erc677/ERC677Token.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.4.15;
+
+import "./ERC677Receiver.sol";
+import "../../zeppelin/token/StandardToken.sol";
+
+contract ERC677Token is StandardToken {
+    function transferAndCall(address receiver, uint amount, bytes data) returns (bool success) {
+        require(transfer(receiver, amount));
+        return _postTransferCall(receiver, amount, data);
+    }
+
+    function _postTransferCall(address receiver, uint amount, bytes data) internal returns (bool success) {
+        return ERC677Receiver(receiver).tokenFallback(msg.sender, amount, data);
+    }
+}
