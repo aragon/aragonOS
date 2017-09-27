0e646424c096dbf73ce67a2ad65541b6477e19d1
JG Carvalho <jgcarvalho93@gmail.com>
2017-09-27 15:45:35 -0300

    added tests for vault contract

---
+++ test/apps_vault.js
@@ -0,0 +1,35 @@
+const { assertInvalidOpcode } = require('./helpers/assertThrow')
+const Vault = artifacts.require('Vault')
+const MiniMeToken = artifacts.require('MiniMeToken')
+
+const n = '0x00';
+
+contract('Vault app', accounts => {
+  let vault, token = {}
+
+  before(async () => {
+      vault = await Vault.new()
+      token = await MiniMeToken.new(n, n, 0, 'n', 0, 'n', true);
+
+      await token.generateTokens(vault.address, 20)
+
+  })
+
+  it('can request allowance', async () => {
+    await vault.requestAllowances([token.address], [5], {from: accounts[1]})
+    assert.equal( await token.allowance(vault.address, accounts[1]), 5, 'should have set an allowance');
+  })
+
+  it('can handle overriding an allowance', async() => {
+    await vault.requestAllowances([token.address], [10], {from: accounts[1]})
+    assert.equal( await token.allowance(vault.address, accounts[1]), 10, 'should have set an allowance');
+  })
+
+  it('can transfer tokens', async () => {
+    await vault.transfer(token.address, accounts[2], 10);
+
+    assert.equal(await token.balanceOf(vault.address), 10, 'should have trasfered')
+    assert.equal(await token.balanceOf(accounts[2]), 10, 'should have trasnfered')
+  })
+
+})
