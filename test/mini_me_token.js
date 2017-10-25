60b9a55f020ddfca2dca60fe93db6e42b2d0f5ca
Chris Purta <cpurta@kochava.com>
2017-10-25 14:48:46 -0700

    Implement testing of MiniMeToken functions

--- test/mini_me_token.js
+++ test/mini_me_token.js
@@ -1,16 +1,16 @@
-const { getBlockNumber, getBlock } = require('./helpers/web3')
+const { getBlockNumber, getBlock, getBalance } = require('./helpers/web3')
+const { assertInvalidOpcode } = require('./helpers/assertThrow')
 const MiniMeToken = artifacts.require('MiniMeToken')
 const MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory')
 
 contract('MiniMeToken', accounts => {
+    const from = accounts[0]
     let factory = {}
     let token = {}
     let clone1 = {}
     let clone2 = {}
 
-    let baseBlock
-
-    beforeEach(async () => {
+    it('should deploy contracts', async () => {
         factory = await MiniMeTokenFactory.new()
         token = await MiniMeToken.new(
             factory.address,
@@ -19,63 +19,73 @@ contract('MiniMeToken', accounts => {
             'MiniMe Test Token',
             18,
             'MMT',
-            true,
-        )
-        await token.generateTokens(token.address, 100)
-        await token.changeController(0xbeef)
-        baseBlock = await getBlockNumber()
+            true)
+
+        assert.ok(token.address)
     })
 
-    it('should have tokens', async () => {
-        assert.equal(await token.balanceOf(token.address), 100)
+    context('create and destroy tokens', () => {
+        it('should generate tokens', async () => {
+            await token.generateTokens(accounts[1], 100)
+            assert.equal(await token.totalSupply(), 100, 'total supply generated should be 100')
+            assert.equal(await token.balanceOf(accounts[1]), 100, 'accounts[1] balance should be 100')
+        })
+
+        it('should be able to destroy tokens', async() => {
+            await token.destroyTokens(accounts[1], 20)
+
+            let block = await getBlockNumber()
+
+            assert.equal(await token.totalSupply(), 80, 'total supply should be at 80')
+            assert.equal(await token.totalSupplyAt(block - 1), 100, 'total supply should be 100 in previous block')
+            assert.equal(await token.balanceOf(accounts[1]), 80, 'should have destroyed 20 tokens from orignal amount')
+        })
     })
 
-    it('should be able to clone token', async () => {
-        clone1 = await MiniMeToken.new(await token.createCloneToken('MMT2',18,'MMT2',baseBlock,true))
+    context('test multi-transfer and disabling', () => {
+        it('token should be able to transfer from account 1 to account 2', async () => {
+            await token.transferFrom(accounts[1], accounts[2], 10)
 
-        // await clone1.changeController(0xbeef)
-        // assert.equal(await clone1.balanceOf(clone1.address), 100, 'should have tokens in cloned token')
-        // assert.equal(await clone1.balanceOfAt(clone1.address), getBlockNumber() - 1, 100, 'should have correct balance beofre creating it')
+            let block = await getBlockNumber()
+
+            assert.equal(await token.totalSupply(), 80, 'total supply should still be at 80')
+            assert.equal(await token.balanceOf(accounts[1]), 70, 'accounts[1] should have updated balance of 60')
+            assert.equal(await token.balanceOf(accounts[2]), 10, 'accounts[2] should have a balance of 10')
+            assert.equal(await token.balanceOfAt(accounts[1], block - 1), 80, 'accounts[1] balance should be 80 in previous block')
+        })
+
+        it('token should be able to transfer from account 2 to account 3', async () => {
+            await token.transferFrom(accounts[2], accounts[3], 5)
+
+            let block = await getBlockNumber()
+
+            assert.equal(await token.totalSupply(), 80, 'total supply should still be at 80')
+            assert.equal(await token.balanceOf(accounts[2]), 5, 'accounts[2] should have updated balance of 5')
+            assert.equal(await token.balanceOf(accounts[3]), 5, 'accounts[3] should have a balance of 5')
+            assert.equal(await token.balanceOfAt(accounts[2], block - 1), 10, 'accounts[2] balance should be 10 in previous block')
+        })
+
+        it('check transfer from controller', async () => {
+            await token.transfer(accounts[2], 5)
+            assert.equal(await token.balanceOf(accounts[2]), 5, 'accounts[2] should now have 10 tokens')
+        })
+
+        it('disable transfers', async () => {
+            token.enableTransfers(false)
+            return assertInvalidOpcode(async () => {
+                await token.transfer(accounts[3], 5)
+            })
+        })
     })
 
-    // it('token should be able to transfer', async () => {
-    //     await token.transfer(0x1, 10)
-    //
-    //     assert.equal(await token.balanceOf(token.address), 90, 'should have updated balance in token')
-    //     assert.equal(await token.balanceOfAt(token.address), getBlockNumber() - 1, 100, 'should have previous balance in token')
-    //     assert.equal(await clone1.balanceOf(clone1.address), 100, 'should have previous balance in cloned token')
-    // })
 
-    // it('should be able to clone token after a transfer', async () => {
-    //     clone2 = await MiniMeToken(token.createCloneToken(
-    //         "MMT3",
-    //         18,
-    //         "MMT3",
-    //         getBlockNumber(),
-    //         true
-    //     ))
-    //     await clone2.changeController(0xbeef); // so it doesn't ask this for callbacks
-    //
-    //     assert.equal(await clone2.balanceOf(clone2.address), 90, 'should have updated balance in token');
-    //     assert.equal(await clone2.balanceOfAt(clone2.address), getBlockNumber() - 2, 100, 'should have previous balance in token');
-    //
-    //     await clone1.transfer(0x1, 10);
-    //     assert.equal(await clone1.balanceOf(clone1.address), 90, 'should have updated balance in token');
-    // })
-    //
-    // it('test that we are able to clone recurrently', async () => {
-    //     let lastClone = clone1
-    //     for (let i = 0; i < 10; i++) {
-    //       lastClone = MiniMeToken(await lastClone.createCloneToken("MMTn", 18, "MMTn", getBlockNumber(), true))
-    //     }
-    //
-    //     await lastClone.changeController(0xbeef) // so it doesn't ask this for callbacks
-    //
-    //     assert.equal(await lastClone.balanceOf(lastClone.address), 90, 'should have updated balance in token');
-    //     assert.equal(await lastClone.balanceOfAt(lastClone.address), baseBlock, 100, 'should be able to travel back in time');
-    // })
+    context('test all cloning', () => {
+        it('should be able to clone token', async () => {
+            clone1 = await MiniMeToken.new(await token.createCloneToken("MMT2", 18, "MMT2", 0, false))
 
-    // it('test multi-transfers', async () => {
-    //
-    // })
+            let curr_block = await getBlockNumber()
+
+            assert.equal(await clone1.totalSupply(), 0, 'should have 0 tokens')
+        })
+    })
 })
