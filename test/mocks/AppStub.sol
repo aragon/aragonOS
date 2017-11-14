9e4bd16f4a04f9176af0f0fbc8e79c498e230b57
Jorge Izquierdo <jorge@izqui.me>
2017-11-14 19:14:36 +0100

    Add ability to execute initialization payload on AppProxy deployment

--- test/mocks/AppStub.sol
+++ test/mocks/AppStub.sol
@@ -4,10 +4,17 @@ import "../../contracts/apps/App.sol";
 
 contract AppSt {
     uint a;
+    bool public initialized;
 }
 
 contract AppStub is App, AppSt {
     bytes32 constant public ROLE = bytes32(1);
+
+    function initialize() {
+        require(!initialized);
+        initialized = true;
+    }
+
     function setValue(uint i) auth(ROLE) {
         a = i;
     }
