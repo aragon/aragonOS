26d4a5e23ee566c5958bbb1738397cfba0d13c73
Jorge Izquierdo <jorge@izqui.me>
2017-12-05 11:25:17 +0100

    Change forwarder methods visibility from external to public

--- contracts/common/IForwarder.sol
+++ contracts/common/IForwarder.sol
@@ -3,6 +3,6 @@ pragma solidity 0.4.15;
 contract IForwarder {
     function isForwarder() public constant returns (bool) { return true; }
 
-    function canForward(address _sender, bytes _evmCallScript) external constant returns (bool);
-    function forward(bytes _evmCallScript) external;
+    function canForward(address _sender, bytes _evmCallScript) public constant returns (bool);
+    function forward(bytes _evmCallScript) public;
 }
