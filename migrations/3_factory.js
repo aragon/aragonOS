7f136b2f6804e0b3c366fb126cbc4d6cbe262036
Jorge Izquierdo <jorge@izqui.me>
2018-01-11 09:16:29 +0100

    Full migrations

--- migrations/3_factory.js
+++ migrations/3_factory.js
@@ -4,7 +4,6 @@ const Kernel = artifacts.require('Kernel')
 const KernelProxy = artifacts.require('KernelProxy')
 
 module.exports = async (deployer, network) => {
-    return
     deployer.deploy(Kernel)
     deployer.deploy(KernelProxy, Kernel.address)
 }
