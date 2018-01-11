9f25ab0bc6e3ea45819f9044b24ca48c68de9a8a
Brett Sun <qisheng.brett.sun@gmail.com>
2018-01-11 12:39:17 -0700

    Add comments to Initializable implementation (#179)

--- contracts/common/Initializable.sol
+++ contracts/common/Initializable.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.18;
 
 
 contract Initializable {
+    // Kept private to protect against modifications from subclasses
     uint256 private initializationBlock;
 
     modifier onlyInit {
@@ -23,6 +24,11 @@ contract Initializable {
         initializationBlock = getBlockNumber();
     }
 
+    /**
+    * @dev Returns the current block number.
+    *      Using a function rather than `block.number` allows us to easily mock the block number in
+    *      tests.
+    */
     function getBlockNumber() internal view returns (uint256) {
         return block.number;
     }
