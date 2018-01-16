ae384bc60f2529e7aae57a112c3a23067bd13d33
Jorge Izquierdo <jorge@izqui.me>
2018-01-16 13:16:23 +0100

    Fix

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     norpc: true,
-    testCommand: 'node --max-old-space-size=0 ../node_modules/.bin/truffle test --network coverage',
+    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     skipFiles: require('glob').sync('contracts/lib/**/*.sol').map(n => n.replace('contracts/', '')),
     copyNodeModules: true,
 }
