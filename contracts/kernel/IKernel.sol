75faad0c2b18288dee5096609984a2fcb6d2e2fd
Brett Sun <qisheng.brett.sun@gmail.com>
2018-01-24 00:17:12 -0500

    IKernel: Add name to indexed event params for SetApp

--- contracts/kernel/IKernel.sol
+++ contracts/kernel/IKernel.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "./IACL.sol";
 
 interface IKernel {
-    event SetApp(bytes32 indexed namespace, bytes32 name, bytes32 indexed id, address indexed app);
+    event SetApp(bytes32 indexed namespace, bytes32 indexed name, bytes32 indexed id, address indexed app);
 
     function acl() public view returns (IACL);
     function hasPermission(address who, address where, bytes32 what, bytes how) public view returns (bool);
