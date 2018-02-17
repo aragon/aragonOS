9d2023d446bfe31a1c630c415ee94b7c82d55e1f
Chris Purta <cpurta@kochava.com>
2018-02-16 17:04:12 -0800

    Update standard token tests to use common helper assertRevert

--- test/zeppelin_StandardToken.js
+++ test/zeppelin_StandardToken.js
@@ -1,8 +1,5 @@
 'use strict';
-
-const assertJump = function(error) {
-  assert.isAbove(error.message.search('revert'), -1, 'Revert should have occurred');
-}
+const { assertRevert } = require('./helpers/assertThrow')
 
 var StandardTokenMock = artifacts.require('./helpers/StandardTokenMock.sol');
 
@@ -40,12 +37,9 @@ contract('StandardToken', function(accounts) {
 
   it('should throw an error when trying to transfer more than balance', async function() {
     let token = await StandardTokenMock.new(accounts[0], 100);
-    try {
+    return assertRevert(async () => {
       await token.transfer(accounts[1], 101);
-      assert.fail('should have thrown before');
-    } catch(error) {
-      assertJump(error);
-    }
+    })
   });
 
   it('should return correct balances after transfering from another account', async function() {
@@ -65,23 +59,17 @@ contract('StandardToken', function(accounts) {
 
   it('should throw an error when trying to transfer more than allowed', async function() {
     await token.approve(accounts[1], 99);
-    try {
+    return assertRevert(async () => {
       await token.transferFrom(accounts[0], accounts[2], 100, {from: accounts[1]});
-      assert.fail('should have thrown before');
-    } catch (error) {
-      assertJump(error);
-    }
+    })
   });
 
   it('should throw an error when trying to transferFrom more than _from has', async function() {
     let balance0 = await token.balanceOf(accounts[0]);
     await token.approve(accounts[1], 99);
-    try {
+    return assertRevert(async () => {
       await token.transferFrom(accounts[0], accounts[2], balance0+1, {from: accounts[1]});
-      assert.fail('should have thrown before');
-    } catch (error) {
-      assertJump(error);
-    }
+    })
   });
 
   describe('validating allowance updates to spender', function() {
@@ -112,23 +100,17 @@ contract('StandardToken', function(accounts) {
 
   it('should throw an error when trying to transfer to 0x0', async function() {
     let token = await StandardTokenMock.new(accounts[0], 100);
-    try {
-      let transfer = await token.transfer(0x0, 100);
-      assert.fail('should have thrown before');
-    } catch(error) {
-      assertJump(error);
-    }
+    return assertRevert(async () => {
+      await token.transferFrom(accounts[0], accounts[2], balance0+1, {from: accounts[1]});
+    })
   });
 
   it('should throw an error when trying to transferFrom to 0x0', async function() {
     let token = await StandardTokenMock.new(accounts[0], 100);
     await token.approve(accounts[1], 100);
-    try {
+    return assertRevert(async () => {
       let transfer = await token.transferFrom(accounts[0], 0x0, 100, {from: accounts[1]});
-      assert.fail('should have thrown before');
-    } catch(error) {
-      assertJump(error);
-    }
+    })
   });
 
 });
