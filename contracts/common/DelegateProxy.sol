9605762c7139bc707a52f42a9ad7bf51fc235439
Jorge Izquierdo <jorge@izqui.me>
2018-03-14 10:30:10 +0100

    Implement min return size in delegateFwd

--- contracts/common/DelegateProxy.sol
+++ contracts/common/DelegateProxy.sol
@@ -8,19 +8,36 @@ contract DelegateProxy {
     * @param _calldata Calldata for the delegatecall
     */
     function delegatedFwd(address _dst, bytes _calldata) internal {
-        require(isContract(_dst));
-        assembly {
-            let result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
-            let size := returndatasize
+        delegateFwd(_dst, _calldata, 0);
+    }
 
-            let ptr := mload(0x40)
-            returndatacopy(ptr, 0, size)
+    /**
+    * @dev Performs a delegatecall and returns whatever the delegatecall returned (entire context execution will return!)
+    * @param _dst Destination address to perform the delegatecall
+    * @param _calldata Calldata for the delegatecall
+    * @param _minReturnSize Minimum size the call needs to return, if less than that it will revert
+    */
+    function delegateFwd(address _dst, bytes _calldata, uint256 _minReturnSize) internal {
+      require(isContract(_dst));
+      uint256 size;
+      uint256 result;
 
-            // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
-            // if the call returned error data, forward it
-            switch result case 0 { revert(ptr, size) }
-            default { return(ptr, size) }
-        }
+      assembly {
+          result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
+          size := returndatasize
+      }
+
+      require(size >= _minReturnSize);
+
+      assembly {
+          let ptr := mload(0x40)
+          returndatacopy(ptr, 0, size)
+
+          // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
+          // if the call returned error data, forward it
+          switch result case 0 { revert(ptr, size) }
+          default { return(ptr, size) }
+      }
     }
 
     function isContract(address _target) internal view returns (bool) {
