aecd6f7857302123ee09f9b13f74520a23511215
Jorge Izquierdo <jorge@izqui.me>
2018-03-14 11:57:17 +0100

    Fix indent

--- contracts/common/DelegateProxy.sol
+++ contracts/common/DelegateProxy.sol
@@ -18,26 +18,26 @@ contract DelegateProxy {
     * @param _minReturnSize Minimum size the call needs to return, if less than that it will revert
     */
     function delegateFwd(address _dst, bytes _calldata, uint256 _minReturnSize) internal {
-      require(isContract(_dst));
-      uint256 size;
-      uint256 result;
+        require(isContract(_dst));
+        uint256 size;
+        uint256 result;
 
-      assembly {
-          result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
-          size := returndatasize
-      }
+        assembly {
+            result := delegatecall(sub(gas, 10000), _dst, add(_calldata, 0x20), mload(_calldata), 0, 0)
+            size := returndatasize
+        }
 
-      require(size >= _minReturnSize);
+        require(size >= _minReturnSize);
 
-      assembly {
-          let ptr := mload(0x40)
-          returndatacopy(ptr, 0, size)
+        assembly {
+            let ptr := mload(0x40)
+            returndatacopy(ptr, 0, size)
 
-          // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
-          // if the call returned error data, forward it
-          switch result case 0 { revert(ptr, size) }
-          default { return(ptr, size) }
-      }
+            // revert instead of invalid() bc if the underlying call failed with invalid() it already wasted gas.
+            // if the call returned error data, forward it
+            switch result case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
     }
 
     function isContract(address _target) internal view returns (bool) {
