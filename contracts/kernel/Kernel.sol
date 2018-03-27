ad8244f7219660216e33e4e7ffe1359e98d0f7e2
ßingen <bingen@aragon.one>
2018-03-27 17:03:41 +0200

    Add address 0 check to isContract
    To save one extcodesize in that case.

--- contracts/kernel/Kernel.sol
+++ contracts/kernel/Kernel.sol
@@ -114,6 +114,10 @@ contract Kernel is IKernel, KernelStorage, Initializable, AppProxyFactory, ACLSy
     }
 
     function isContract(address _target) internal view returns (bool) {
+        if (_target == address(0)) {
+            return false;
+        }
+
         uint256 size;
         assembly { size := extcodesize(_target) }
         return size > 0;
