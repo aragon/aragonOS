f4c05278342706c051fbfb0490a27c96af8e5e40
ßingen <bingen@aragon.one>
2018-04-03 22:56:00 +0200

    Fix incorrect appId constants
    Closes #282.

--- contracts/evmscript/IEVMScriptRegistry.sol
+++ contracts/evmscript/IEVMScriptRegistry.sol
@@ -2,8 +2,17 @@ pragma solidity 0.4.18;
 
 
 contract EVMScriptRegistryConstants {
-    bytes32 constant public EVMSCRIPT_REGISTRY_APP_ID = keccak256("evmreg.aragonpm.eth");
-    bytes32 constant public EVMSCRIPT_REGISTRY_APP = keccak256(keccak256("app"), EVMSCRIPT_REGISTRY_APP_ID);
+    // repeated definitions from KernelStorage, to avoid out of gas issues
+    bytes32 constant public ETH_NODE = keccak256(keccak256(0), keccak256("eth"));
+    bytes32 constant public APM_NODE = keccak256(ETH_NODE, keccak256("aragonpm"));
+    bytes32 constant public APP_ADDR_NAMESPACE = keccak256("app");
+
+    bytes32 constant public EVMSCRIPT_REGISTRY_APP_ID = apmNameHash("evmreg");
+    bytes32 constant public EVMSCRIPT_REGISTRY_APP = keccak256(APP_ADDR_NAMESPACE, EVMSCRIPT_REGISTRY_APP_ID);
+
+    function apmNameHash(string name) internal pure returns (bytes32) {
+        return keccak256(APM_NODE, keccak256(name));
+    }
 }
 
 
