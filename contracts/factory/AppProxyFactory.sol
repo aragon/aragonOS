c78d546316e185558ff24a7831b647f131da3c8e
Brett Sun <qisheng.brett.sun@gmail.com>
2018-05-14 02:40:05 -0700

    AppProxyFactory: emit appId in NewAppProxy event

--- contracts/factory/AppProxyFactory.sol
+++ contracts/factory/AppProxyFactory.sol
@@ -5,7 +5,7 @@ import "../apps/AppProxyPinned.sol";
 
 
 contract AppProxyFactory {
-    event NewAppProxy(address proxy, bool isUpgradeable);
+    event NewAppProxy(address proxy, bool isUpgradeable, bytes32 appId);
 
     function newAppProxy(IKernel _kernel, bytes32 _appId) public returns (AppProxyUpgradeable) {
         return newAppProxy(_kernel, _appId, new bytes(0));
@@ -13,7 +13,7 @@ contract AppProxyFactory {
 
     function newAppProxy(IKernel _kernel, bytes32 _appId, bytes _initializePayload) public returns (AppProxyUpgradeable) {
         AppProxyUpgradeable proxy = new AppProxyUpgradeable(_kernel, _appId, _initializePayload);
-        NewAppProxy(address(proxy), true);
+        NewAppProxy(address(proxy), true, _appId);
         return proxy;
     }
 
@@ -23,7 +23,7 @@ contract AppProxyFactory {
 
     function newAppProxyPinned(IKernel _kernel, bytes32 _appId, bytes _initializePayload) public returns (AppProxyPinned) {
         AppProxyPinned proxy = new AppProxyPinned(_kernel, _appId, _initializePayload);
-        NewAppProxy(address(proxy), false);
+        NewAppProxy(address(proxy), false, _appId);
         return proxy;
     }
 }
