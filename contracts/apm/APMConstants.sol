e3637df35a3ebda43afd4bbb91baf3c17a2349b5
Brett Sun <qisheng.brett.sun@gmail.com>
2018-05-16 17:18:05 -0700

    Make APMNamehash public

--- contracts/apm/APMConstants.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.18;
-
-import "../ens/ENSConstants.sol";
-
-
-contract APMConstants is ENSConstants {
-    bytes32 constant public APM_NODE = keccak256(ETH_TLD_NODE, keccak256("aragonpm"));
-}
