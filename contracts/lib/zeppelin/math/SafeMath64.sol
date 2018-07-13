777ca88a172f2c9255a1432ebbe407d914610777
Brett Sun <qisheng.brett.sun@gmail.com>
2018-07-13 16:43:20 +0200

    Update SafeMath libraries to their current gas-optimized versions

--- contracts/lib/zeppelin/math/SafeMath64.sol
+++ contracts/lib/zeppelin/math/SafeMath64.sol
@@ -1,4 +1,7 @@
-pragma solidity ^0.4.11;
+// See https://github.com/OpenZeppelin/openzeppelin-solidity/blob/40b5594f52fce22f6c9190e8e45ccb3cab624783/contracts/math/SafeMath.sol
+// Adapted for uint64, pragma ^0.4.18, and satisfying our linter rules
+
+pragma solidity ^0.4.18;
 
 
 /**
@@ -6,27 +9,47 @@ pragma solidity ^0.4.11;
  * @dev Math operations for uint64 with safety checks that throw on error
  */
 library SafeMath64 {
-  function mul(uint64 a, uint64 b) internal pure returns (uint64) {
-    uint64 c = a * b;
-    require(a == 0 || c / a == b);
-    return c;
-  }
 
-  function div(uint64 a, uint64 b) internal pure returns (uint64) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint64 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint64 a, uint64 b) internal pure returns (uint64 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
 
-  function sub(uint64 a, uint64 b) internal pure returns (uint64) {
-    require(b <= a);
-    return a - b;
-  }
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-  function add(uint64 a, uint64 b) internal pure returns (uint64) {
-    uint64 c = a + b;
-    require(c >= a);
-    return c;
-  }
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint64 a, uint64 b) internal pure returns (uint64) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint64 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint64 a, uint64 b) internal pure returns (uint64) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint64 a, uint64 b) internal pure returns (uint64 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
--- contracts/lib/zeppelin/math/SafeMath8.sol
+++ contracts/lib/zeppelin/math/SafeMath8.sol
@@ -1,32 +1,55 @@
-pragma solidity ^0.4.11;
+// See https://github.com/OpenZeppelin/openzeppelin-solidity/blob/40b5594f52fce22f6c9190e8e45ccb3cab624783/contracts/math/SafeMath.sol
+// Adapted for uint8, pragma ^0.4.18, and satisfying our linter rules
+
+pragma solidity ^0.4.18;
 
 
 /**
- * @title SafeMath8
+ * @title SafeMath64
  * @dev Math operations for uint8 with safety checks that throw on error
  */
 library SafeMath8 {
-  function mul(uint8 a, uint8 b) internal pure returns (uint8) {
-    uint8 c = a * b;
-    require(a == 0 || c / a == b);
-    return c;
-  }
 
-  function div(uint8 a, uint8 b) internal pure returns (uint8) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint8 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint8 a, uint8 b) internal pure returns (uint8 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
 
-  function sub(uint8 a, uint8 b) internal pure returns (uint8) {
-    require(b <= a);
-    return a - b;
-  }
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
-  function add(uint8 a, uint8 b) internal pure returns (uint8) {
-    uint8 c = a + b;
-    require(c >= a);
-    return c;
-  }
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint8 a, uint8 b) internal pure returns (uint8) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint8 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint8 a, uint8 b) internal pure returns (uint8) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint8 a, uint8 b) internal pure returns (uint8 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
