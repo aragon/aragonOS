5b9222850aa47b98c1591723d32ec6eaf5584df3
Brett Sun <qisheng.brett.sun@gmail.com>
2018-07-30 14:39:39 +0200

    ACL: leave gap in param set instead of collapsing

--- contracts/acl/ACL.sol
+++ contracts/acl/ACL.sol
@@ -34,9 +34,10 @@ contract ACL is IACL, AragonApp, ACLHelpers {
 
     uint8 constant BLOCK_NUMBER_PARAM_ID = 200;
     uint8 constant TIMESTAMP_PARAM_ID    = 201;
-    uint8 constant ORACLE_PARAM_ID       = 202;
-    uint8 constant LOGIC_OP_PARAM_ID     = 203;
-    uint8 constant PARAM_VALUE_PARAM_ID  = 204;
+    // 202 is unused
+    uint8 constant ORACLE_PARAM_ID       = 203;
+    uint8 constant LOGIC_OP_PARAM_ID     = 204;
+    uint8 constant PARAM_VALUE_PARAM_ID  = 205;
     // TODO: Add execution times param type?
 
     // Hardcoded constant to save gas
