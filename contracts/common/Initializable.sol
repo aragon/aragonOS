5dce5d60badc7daccaedc177d575fa547e5e88ee
ßingen <bingen@aragon.one>
2018-07-30 13:22:03 +0200

    Add timestamp getter to Initializable and 64 bit version for block

--- contracts/common/Initializable.sol
+++ contracts/common/Initializable.sol
@@ -40,4 +40,31 @@ contract Initializable is AppStorage {
     function getBlockNumber() internal view returns (uint256) {
         return block.number;
     }
+
+    /**
+    * @dev Returns the current block number, converted to uint64.
+    *      Using a function rather than `block.number` allows us to easily mock the block number in
+    *      tests.
+    */
+    function getBlockNumber64() internal view returns (uint64) {
+        return uint64(block.number);
+    }
+
+    /**
+    * @dev Returns the current timestamp.
+    *      Using a function rather than `now` allows us to easily mock it in
+    *      tests.
+    */
+    function getTimestamp() internal view returns (uint256) {
+        return now;
+    }
+
+    /**
+    * @dev Returns the current timestamp, covnerted to uint64.
+    *      Using a function rather than `now` allows us to easily mock it in
+    *      tests.
+    */
+    function getTimestamp64() internal view returns (uint64) {
+        return uint64(now);
+    }
 }
