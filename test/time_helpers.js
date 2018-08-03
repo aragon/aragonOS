b36187aa4fec0107085bb1b452c4094f28128b55
ßingen <bingen@aragon.one>
2018-08-03 11:51:47 +0200

    Time helpers: add test
    Address PR 375 comments.

--- test/time_helpers.js
+++ test/time_helpers.js
@@ -6,10 +6,12 @@ contract('TimeHelpers test', accounts => {
   })
 
   it('checks block number', async () => {
-    assert.equal((await timeHelpersMock.getBlockNumberExt.call()).toString(), (await timeHelpersMock.getBlockNumber64Ext.call()).toString(), "block number should match")
+    assert.equal((await timeHelpersMock.getBlockNumberExt.call()).toString(), (await timeHelpersMock.getBlockNumber64Ext.call()).toString(), "block numbers should match")
+    assert.equal((await timeHelpersMock.getBlockNumberExt.call()).toString(), (await timeHelpersMock.getBlockNumberDirect.call()).toString(), web3.eth.blockNumber, "block number should match with real one", "block number should match with real one")
   })
 
   it('checks time stamp', async () => {
-    assert.equal((await timeHelpersMock.getTimestampExt.call()).toString(), (await timeHelpersMock.getTimestamp64Ext.call()).toString(), "time stamp should match")
+    assert.equal((await timeHelpersMock.getTimestampExt.call()).toString(), (await timeHelpersMock.getTimestamp64Ext.call()).toString(), "time stamps should match")
+    assert.equal((await timeHelpersMock.getTimestampExt.call()).toString(), (await timeHelpersMock.getTimestampDirect.call()).toString(), "time stamp should match with real one")
   })
 })
