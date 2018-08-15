796ebfd3da3cf4d7de152d0933be80960f08b77b
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-15 10:38:06 +0200

    ACL: log whether a permission was set with parameters (#362)
    Fixes #212.

--- test/kernel_acl.js
+++ test/kernel_acl.js
@@ -1,5 +1,6 @@
 const { assertRevert } = require('./helpers/assertThrow')
 const { getBlockNumber } = require('./helpers/web3')
+const { soliditySha3 } = require('web3-utils')
 const assertEvent = require('./helpers/assertEvent')
 
 const DAOFactory = artifacts.require('DAOFactory')
@@ -87,6 +88,7 @@ contract('Kernel ACL', accounts => {
         beforeEach(async () => {
             const receipt = await acl.createPermission(granted, app, role, granted, { from: permissionsRoot })
             assertEvent(receipt, 'SetPermission')
+            assertEvent(receipt, 'SetPermissionParams', 0) // should not have emitted this
             assertEvent(receipt, 'ChangePermissionManager')
         })
 
@@ -108,6 +110,12 @@ contract('Kernel ACL', accounts => {
             assert.equal(returnedParam[1].valueOf(), parseInt(op, 10), 'param op should match')
             assert.equal(returnedParam[2].valueOf(), parseInt(value, 10), 'param value should match')
 
+            // Assert that the right events have been emitted with the right args
+            assertEvent(r1, 'SetPermission')
+            assertEvent(r1, 'SetPermissionParams')
+            const setParamsHash = r1.logs.filter(l => l.event == 'SetPermissionParams')[0].args.paramsHash
+            assert.equal(setParamsHash, soliditySha3(param))
+
             // grants again without re-saving params
             const r2 = await acl.grantPermissionP(accounts[4], app, role, [param], { from: granted })
 
@@ -126,7 +134,10 @@ contract('Kernel ACL', accounts => {
         it('can grant a public permission', async () => {
             const anyEntity = "0xffffffffffffffffffffffffffffffffffffffff"
 
-            await acl.grantPermission(anyEntity, app, role, { from: granted })
+            const receipt = await acl.grantPermission(anyEntity, app, role, { from: granted })
+            assertEvent(receipt, 'SetPermission')
+            assertEvent(receipt, 'SetPermissionParams', 0) // should not have emitted this
+
             // many entities can succesfully perform action
             // acl is used here just to provide an address which is a contract
             await kernel.setApp('0x121212', '0x00', acl.address, { from: accounts[4] })
