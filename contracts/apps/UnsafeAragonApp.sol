ca053567e2e18c555fead486fb8b496405763ceb
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-16 12:20:49 +0200

    Adjust comments

--- contracts/apps/UnsafeAragonApp.sol
+++ contracts/apps/UnsafeAragonApp.sol
@@ -10,9 +10,9 @@ import "./AragonApp.sol";
 // Using UnsafeAragonApp means you'll be playing with 🔥.
 // A number of safe defaults are provided with AragonApp, to help you avoid dangerous situations
 // and mistakes with how your contract's developed as well as how it's deployed.
-// UnsafeAragonApp turns off these safety features to give you greater control over you contract.
+// UnsafeAragonApp turns off these safety features to give you greater control over your contract.
 // In particular, it allows you to:
-//   - Directly use deployed base contracts as apps, without a proxy
+//   - Use deployed base contracts as apps directly, without a proxy
 contract UnsafeAragonApp is AragonApp {
     function UnsafeAragonApp() public {
         // Removes auto petrifying
