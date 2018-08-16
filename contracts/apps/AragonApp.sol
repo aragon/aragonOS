1d2d9ad15be1aad0ea8b2bb8418f63051a027810
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-16 12:16:23 +0200

    AragonApp: adjust inheritance order so that ACLSyntaxSugar comes last

--- contracts/apps/AragonApp.sol
+++ contracts/apps/AragonApp.sol
@@ -16,7 +16,7 @@ import "../acl/ACLSyntaxSugar.sol";
 // Unless overriden, this behaviour enforces those contracts to be usable only behind an AppProxy.
 // ACLSyntaxSugar and EVMScriptRunner are not directly used by this contract, but are included so
 // that they are automatically usable by subclassing contracts
-contract AragonApp is AppStorage, Autopetrified, ACLSyntaxSugar, VaultRecoverable, EVMScriptRunner {
+contract AragonApp is AppStorage, Autopetrified, VaultRecoverable, EVMScriptRunner, ACLSyntaxSugar {
     modifier auth(bytes32 _role) {
         require(canPerform(msg.sender, _role, new uint256[](0)));
         _;
