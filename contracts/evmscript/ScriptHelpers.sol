97b835b9d2614ed30e00f416b5b4397922823d5c
Jorge Izquierdo <jorge@izqui.me>
2018-08-17 22:03:45 +0200

    Remove unused memcpy from ScriptHelpers

--- contracts/evmscript/ScriptHelpers.sol
+++ contracts/evmscript/ScriptHelpers.sol
@@ -45,27 +45,4 @@ library ScriptHelpers {
         assembly { mstore(add(payload, 0x20), _sig) }
         return payload;
     }
-
-    function memcpy(uint _dest, uint _src, uint _len) internal pure {
-        uint256 src = _src;
-        uint256 dest = _dest;
-        uint256 len = _len;
-
-        // Copy word-length chunks while possible
-        for (; len >= 32; len -= 32) {
-            assembly {
-                mstore(dest, mload(src))
-            }
-            dest += 32;
-            src += 32;
-        }
-
-        // Copy remaining bytes
-        uint mask = 256 ** (32 - len) - 1;
-        assembly {
-            let srcpart := and(mload(src), not(mask))
-            let destpart := and(mload(dest), mask)
-            mstore(dest, or(destpart, srcpart))
-        }
-    }
 }
