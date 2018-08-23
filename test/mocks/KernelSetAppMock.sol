9ca9f3fcfd50796b73e9dbedbe66db4148178dee
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-23 10:36:28 +0200

    tests: add comment for KernelSetAppMock

--- test/mocks/KernelSetAppMock.sol
+++ test/mocks/KernelSetAppMock.sol
@@ -6,6 +6,7 @@ contract KernelSetAppMock is Kernel {
     function KernelSetAppMock() Kernel(false) public {
     }
 
+    // Overloaded mock to bypass the auth and isContract checks
     function setApp(bytes32 _namespace, bytes32 _name, address _app) public returns (bytes32 id) {
         id = keccak256(_namespace, _name);
         apps[id] = _app;
