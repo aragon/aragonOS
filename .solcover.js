1a5d332f17266a16a8340ebcd6c2d34f004bb7c9
Jorge Izquierdo <jorge@izqui.me>
2018-08-23 12:17:17 +0200

    Update solidity-coverage settings

--- .solcover.js
+++ .solcover.js
@@ -1,21 +1,10 @@
-// NOTE: Upgrading to solidity-coverage 0.4.x breaks our tests
-
 const libFiles = require('glob').sync('contracts/lib/**/*.sol').map(n => n.replace('contracts/', ''))
-const interfaces = [
-  'acl/IACL.sol',
-  'acl/IACLOracle.sol',
-  'acl/ACLSyntaxSugar.sol',
-  'common/IForwarder.sol',
-  'common/IVaultRecoverable.sol',
-  'evmscript/IEVMScriptExecutor.sol',
-  'kernel/IKernel.sol',
-  'mocks',
-]
+
+const skipFiles = libFiles.concat(['mocks'])
 
 module.exports = {
     norpc: true,
     compileCommand: '../node_modules/.bin/truffle compile',
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    skipFiles: interfaces.concat(libFiles),
-    copyNodeModules: true,
+    skipFiles,
 }
