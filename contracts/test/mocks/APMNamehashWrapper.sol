430358b8e9e94e093a3bcee3ada1359374fedc6c
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-24 19:02:59 +0200

    test: stop emitting events from APMNamehash mock

--- contracts/test/mocks/APMNamehashWrapper.sol
+++ contracts/test/mocks/APMNamehashWrapper.sol
@@ -4,13 +4,8 @@ import "../../apm/APMNamehash.sol";
 
 
 contract APMNamehashWrapper is APMNamehash {
-    event LogHash(string name, bytes32 hash);
-
     function getAPMNamehash(string name) public returns (bytes32 hash) {
         hash = apmNamehash(name);
-        emit LogHash("eth node", ETH_TLD_NODE);
-        emit LogHash("aragonpm.eth", APM_NODE);
-        emit LogHash(name, hash);
         return hash;
     }
 }
