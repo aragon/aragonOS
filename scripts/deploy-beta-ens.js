4a424c5ae794920b54b8f180497dd13e96d2c461
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-04 00:28:26 +0200

    Remove migrations and revamp scripts (#413)
    We don't use these migrations to deploy anything, and they take up time when running tests.
    
    Also revamped the deployment scripts to be more modular.

--- scripts/deploy-beta-ens.js
+++ scripts/deploy-beta-ens.js
@@ -1,19 +1,35 @@
-const ENSFactory = artifacts.require('ENSFactory')
+const globalArtifacts = this.artifacts // Not injected unless called directly via truffle
 
-const owner = process.env.OWNER || '0x4cb3fd420555a09ba98845f0b816e45cfb230983'
+const defaultOwner = process.env.OWNER || '0x4cb3fd420555a09ba98845f0b816e45cfb230983'
 
-module.exports = async callback => {
-  console.log('deploying factory')
+module.exports = async (truffleExecCallback, { artifacts = globalArtifacts, owner = defaultOwner, verbose = true } = {}) => {
+  const log = (...args) => {
+    if (verbose) { console.log(...args) }
+  }
+
+  const ENS = artifacts.require('ENS')
+  const ENSFactory = artifacts.require('ENSFactory')
+
+  log('Deploying ENSFactory...')
   const factory = await ENSFactory.new()
-  console.log('factory deployed', factory.address)
+  log('ENSFactory deployed:', factory.address)
   const receipt = await factory.newENS(owner)
 
-  const ens = receipt.logs.filter(l => l.event == 'DeployENS')[0].args.ens
-  console.log('====================')
-  console.log('Deployed ENS:', ens)
+  const ensAddr = receipt.logs.filter(l => l.event == 'DeployENS')[0].args.ens
+  log('====================')
+  log('Deployed ENS:', ensAddr)
 
-  console.log(ens)
-  callback()
+  log(ensAddr)
+
+  if (typeof truffleExecCallback === 'function') {
+    // Called directly via `truffle exec`
+    truffleExecCallback()
+  } else {
+    return {
+      ens: ENS.at(ensAddr),
+      ensFactory: factory,
+    }
+  }
 }
 
 // Rinkeby ENS: 0xfbae32d1cde62858bc45f51efc8cc4fa1415447e
