fcb67f7b1f17e2423c4c1c6048ea0f4b019c311d
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-06 18:45:25 +0200

    Parameterize deploying EVMScriptRegistryFactory in DAOFactory deployment

--- scripts/deploy-daofactory.js
+++ scripts/deploy-daofactory.js
@@ -1,6 +1,13 @@
 const globalArtifacts = this.artifacts // Not injected unless called directly via truffle
 
-module.exports = async (truffleExecCallback, { artifacts = globalArtifacts, verbose = true } = {}) => {
+module.exports = async (
+  truffleExecCallback,
+  {
+    artifacts = globalArtifacts,
+    withEvmScripts = true,
+    verbose = true
+  } = {}
+) => {
   const log = (...args) => {
     if (verbose) { console.log(...args) }
   }
@@ -9,13 +16,20 @@ module.exports = async (truffleExecCallback, { artifacts = globalArtifacts, verb
   const Kernel = artifacts.require('Kernel')
 
   const DAOFactory = artifacts.require('DAOFactory')
-  const EVMScriptRegistryFactory = artifacts.require('EVMScriptRegistryFactory')
 
   log('Deploying DAOFactory with bases...')
   const kernelBase = await Kernel.new(true) // immediately petrify
   const aclBase = await ACL.new()
-  const evmScriptRegistryFactory = await EVMScriptRegistryFactory.new()
-  const daoFactory = await DAOFactory.new(kernelBase.address, aclBase.address, evmScriptRegistryFactory.address)
+  let evmScriptRegistryFactory
+  if (withEvmScripts) {
+    const EVMScriptRegistryFactory = artifacts.require('EVMScriptRegistryFactory')
+    evmScriptRegistryFactory = await EVMScriptRegistryFactory.new()
+  }
+  const daoFactory = await DAOFactory.new(
+    kernelBase.address,
+    aclBase.address,
+    evmScriptRegistryFactory ? evmScriptRegistryFactory.address : '0x00'
+  )
   log('DAOFactory deployed:', daoFactory.address)
 
   if (typeof truffleExecCallback === 'function') {
