56a609fc8672f83b7a6b21867653c1def545c502
Jorge Izquierdo <izqui97@gmail.com>
2018-10-30 10:19:45 +0100

    Fix web3 global in deploy-apm
    Co-Authored-By: bingen <bingentxu@gmail.com>

--- scripts/deploy-apm.js
+++ scripts/deploy-apm.js
@@ -7,7 +7,7 @@ const logDeploy = require('./helpers/deploy-logger')
 const getAccounts = require('./helpers/get-accounts')
 
 const globalArtifacts = this.artifacts // Not injected unless called directly via truffle
-const globalWeb3 = this.artifacts // Not injected unless called directly via truffle
+const globalWeb3 = this.web3 // Not injected unless called directly via truffle
 
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 
