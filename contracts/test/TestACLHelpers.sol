c44a02546ef0e3602237116f37faa3742df43327
Maxime Cote <maxime.cote184@gmail.com>
2018-11-08 10:44:01 -0500

    Add encodeParams test

--- contracts/test/TestACLHelpers.sol
+++ contracts/test/TestACLHelpers.sol
@@ -23,4 +23,35 @@ contract TestACLHelpers is ACLHelpers, ACL {
         Assert.equal(uint256(param.value), uint256(value), "Encoded value is not equal");
     }
 
+    function testEncodeParams() public {
+        Param[] memory params = new Param[](2);
+
+        params[0] = Param({ 
+            id: 1,
+            op: uint8(Op.EQ),
+            value: 5294967297
+        });
+
+        params[1] = Param({ 
+            id: 2,
+            op: uint8(Op.EQ),
+            value: 5294967297
+        });
+
+
+        uint256[] memory encodedParam = encodeParams(params);
+
+        (uint32 id0, uint32 op0, uint32 value0) = decodeParamsList(encodedParam[0]);
+
+        Assert.equal(uint256(params[0].id), uint256(id0), "Encoded id is not equal");
+        Assert.equal(uint256(params[0].op), uint256(op0), "Encoded op is not equal");
+        Assert.equal(uint256(params[0].value), uint256(value0), "Encoded value is not equal");
+
+        (uint32 id1, uint32 op1, uint32 value1) = decodeParamsList(encodedParam[1]);
+
+        Assert.equal(uint256(params[1].id), uint256(id1), "Encoded id is not equal");
+        Assert.equal(uint256(params[1].op), uint256(op1), "Encoded op is not equal");
+        Assert.equal(uint256(params[1].value), uint256(value1), "Encoded value is not equal");        
+    }    
+
 }
