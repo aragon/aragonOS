b05c010820fa8066922882993157fb647929772c
Maxime Cote <maxime.cote184@gmail.com>
2018-11-19 19:00:22 -0500

    Move Op and Param to ACLParams contract

---
+++ contracts/acl/ACLParams.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+
+contract ACLParams {
+
+    enum Op { NONE, EQ, NEQ, GT, LT, GTE, LTE, RET, NOT, AND, OR, XOR, IF_ELSE } // op types
+
+    struct Param {
+        uint8 id;
+        uint8 op;
+        uint240 value; // even though value is an uint240 it can store addresses
+        // in the case of 32 byte hashes losing 2 bytes precision isn't a huge deal
+        // op and id take less than 1 byte each so it can be kept in 1 sstore
+    }
+}
