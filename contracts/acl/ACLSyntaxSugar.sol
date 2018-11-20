dd2f8f2cb8e2a3a8a64392eddf57665598f73876
Maxime Cote <maxime.cote184@gmail.com>
2018-11-19 23:10:23 -0500

    Expose ACLHelpers functions

--- contracts/acl/ACLSyntaxSugar.sol
+++ contracts/acl/ACLSyntaxSugar.sol
@@ -104,15 +104,15 @@ contract ACLHelpers is ACLParams {
         return uint256(param.id) << 248 | uint256(param.op) << 240 | param.value;
     }
 
-    function decodeParamOp(uint256 _x) internal pure returns (uint8 b) {
+    function decodeParamOp(uint256 _x) public pure returns (uint8 b) {
         return uint8(_x >> (8 * 30));
     }
 
-    function decodeParamId(uint256 _x) internal pure returns (uint8 b) {
+    function decodeParamId(uint256 _x) public pure returns (uint8 b) {
         return uint8(_x >> (8 * 31));
     }
 
-    function decodeParamsList(uint256 _x) internal pure returns (uint32 a, uint32 b, uint32 c) {
+    function decodeParamsList(uint256 _x) public pure returns (uint32 a, uint32 b, uint32 c) {
         a = uint32(_x);
         b = uint32(_x >> (8 * 4));
         c = uint32(_x >> (8 * 8));
