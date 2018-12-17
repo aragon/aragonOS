af698eaf968b5c90016f9f401505f6c671ca8371
Brett Sun <qisheng.brett.sun@gmail.com>
2018-12-17 21:26:18 +0100

    cosmetic: fix indenting for SafeERC20Mock

--- contracts/test/mocks/SafeERC20Mock.sol
+++ contracts/test/mocks/SafeERC20Mock.sol
@@ -8,19 +8,19 @@ contract SafeERC20Mock {
     using SafeERC20 for ERC20;
     event Result(bool result);
 
-	function transfer(ERC20 token, address to, uint256 amount) external returns (bool) {
+    function transfer(ERC20 token, address to, uint256 amount) external returns (bool) {
         bool result = token.safeTransfer(to, amount);
         emit Result(result);
         return result;
-	}
+    }
 
-	function transferFrom(ERC20 token, address from, address to, uint256 amount) external returns (bool) {
+    function transferFrom(ERC20 token, address from, address to, uint256 amount) external returns (bool) {
         bool result = token.safeTransferFrom(from, to, amount);
         emit Result(result);
         return result;
-	}
+    }
 
-	function approve(ERC20 token, address spender, uint256 amount) external returns (bool) {
+    function approve(ERC20 token, address spender, uint256 amount) external returns (bool) {
         bool result = token.safeApprove(spender, amount);
         emit Result(result);
         return result;
