16e236def19c4a16e3e8ac7c0dd0020399c1c760
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-04-24 11:10:37 -0300

    contracts: base kill switch implementation

---
+++ contracts/kill_switch/base/IssuesRegistry.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.4.24;
+
+import "../../apps/AragonApp.sol";
+
+
+contract IssuesRegistry is AragonApp {
+    bytes32 constant public SET_ENTRY_SEVERITY_ROLE = keccak256("SET_ENTRY_SEVERITY_ROLE");
+
+    enum Severity { None, Low, Mid, High, Critical }
+
+    mapping (address => Severity) internal issuesSeverity;
+
+    event SeveritySet(address indexed entry, Severity severity, address sender);
+
+    function initialize() public onlyInit {
+        initialized();
+    }
+
+    function isSeverityFor(address entry) public view isInitialized returns (bool) {
+        return issuesSeverity[entry] != Severity.None;
+    }
+
+    function getSeverityFor(address entry) public view isInitialized returns (Severity) {
+        return issuesSeverity[entry];
+    }
+
+    function setSeverityFor(address entry, Severity severity) authP(SET_ENTRY_SEVERITY_ROLE, arr(entry, msg.sender)) public {
+        issuesSeverity[entry] = severity;
+        emit SeveritySet(entry, severity, msg.sender);
+    }
+}
