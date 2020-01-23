a3f94ef9f03cbee80b148a00894824f7ef87a2c6
William Griffiths <willjgriff@gmail.com>
2020-01-23 10:24:03 +0000

    Add JS test for successful ACL oracle.

--- test/contracts/acl/acl_params.js
+++ test/contracts/acl/acl_params.js
@@ -4,6 +4,7 @@ const { paramForOracle } = require('../../helpers/permissionParams')
 const ACL = artifacts.require('ACL')
 const Kernel = artifacts.require('Kernel')
 const KernelProxy = artifacts.require('KernelProxy')
+const AcceptOracle = artifacts.require('AcceptOracle')
 const OverGasLimitOracle = artifacts.require('OverGasLimitOracle')
 const StateModifyingOracle = artifacts.require('StateModifyingOracle')
 
@@ -25,6 +26,20 @@ contract('ACL', ([permissionsRoot, mockAppAddress]) => {
     await acl.createPermission(permissionsRoot, mockAppAddress, MOCK_APP_ROLE, permissionsRoot)
   })
 
+  it('ACLOracle succeeds when oracle canPerform returns true', async () => {
+    const acceptOracle = await AcceptOracle.new()
+    const param = paramForOracle(acceptOracle.address)
+    await acl.grantPermissionP(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE, [param])
+    assert.isTrue(await acl.hasPermission(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE))
+  })
+
+  it('ACLOracle fails when oracle canPerform modifies state', async () => {
+    const stateModifyingOracle = await StateModifyingOracle.new()
+    const param = paramForOracle(stateModifyingOracle.address)
+    await acl.grantPermissionP(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE, [param])
+    await assertRevert(acl.hasPermission(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE), "ACL_ORACLE_OOG")
+  })
+
   context('> ACLOracle OverGasLimitOracle', () => {
     let overGasLimitOracle, param
 
@@ -43,21 +58,6 @@ contract('ACL', ([permissionsRoot, mockAppAddress]) => {
     it('fails when oracle canPerform goes OOG with specified permission owner', async () => {
       await acl.grantPermissionP(permissionsRoot, mockAppAddress, MOCK_APP_ROLE, [param])
       await assertRevert(acl.hasPermission(permissionsRoot, mockAppAddress, MOCK_APP_ROLE), "ACL_ORACLE_OOG")
-    })
-
-  })
-
-  context('> ACLOracle StateModifyingOracle', () => {
-    let stateModifyingOracle, param
-
-    beforeEach(async () => {
-      stateModifyingOracle = await StateModifyingOracle.new()
-      param = paramForOracle(stateModifyingOracle.address)
-    })
-
-    it('fails when oracle canPerform modifies state', async () => {
-      await acl.grantPermissionP(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE, [param])
-      await assertRevert(acl.hasPermission(ANY_ADDR, mockAppAddress, MOCK_APP_ROLE), "ACL_ORACLE_OOG")
     })
   })
 })
