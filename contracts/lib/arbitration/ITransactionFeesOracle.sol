cd874366d4587e9119978c3501f500de3aedbc7c
ßingen <bingen@aragon.one>
2020-07-01 11:47:55 +0200

    disputable apps: ITransactionFeesOracle enhancements
    - Add payment endpoint
    - Rename methods to be “transaction specific”
    - Add events

--- contracts/lib/arbitration/ITransactionFeesOracle.sol
+++ contracts/lib/arbitration/ITransactionFeesOracle.sol
@@ -4,9 +4,14 @@ import "../token/ERC20.sol";
 
 
 interface ITransactionFeesOracle {
-    function setFee(bytes32 appId, ERC20 token, uint256 amount) external;
-    function setFees(bytes32[] _appIds, ERC20[] _tokens, uint256[] _amounts) external;
-    function unsetFee(bytes32 _appId) external;
-    function unsetFees(bytes32[] _appIds) external;
-    function getFee(bytes32 appId) external view returns (ERC20, uint256, address);
+    event TransactionFeeSet(bytes32 indexed appId, ERC20 token, uint256 amount);
+    event TransactionFeeUnset(bytes32 indexed appId);
+    event TransactionFeePaid(address indexed by, bytes32 appId, uint256 actionId);
+
+    function setTransactionFee(bytes32 _appId, ERC20 _token, uint256 _amount) external;
+    function setTransactionFees(bytes32[] _appIds, ERC20[] _tokens, uint256[] _amounts) external;
+    function unsetTransactionFee(bytes32 _appId) external;
+    function unsetTransactionFees(bytes32[] _appIds) external;
+    function payTransactionFees(bytes32 _appId, uint256 _actionId) external;
+    function getTransactionFee(bytes32 _appId) external view returns (ERC20, uint256, address);
 }
