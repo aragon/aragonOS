cd7857e821919e74ed9bd3872d556423c6f0c626
Brett Sun <qisheng.brett.sun@gmail.com>
2020-07-02 00:32:36 +0200

    ACL: update user/who parameters to who/grantee

--- contracts/acl/IACLOracle.sol
+++ contracts/acl/IACLOracle.sol
@@ -7,18 +7,18 @@ pragma solidity ^0.4.24;
 
 /**
 * @title ACL Oracle interface
-* @dev This interface simply defines a check method that must be implemented by smart contracts to be plugged in as ACL oracles.
-*      ACL oracles are the most suitable way to have external contracts validating ACL permissions with custom logic.
+* @dev This interface simply defines a predicate method that must be implemented by smart contracts intended to be used as ACL Oracles.
+*      ACL oracles should be used if you would like to protect a permission with custom logic from an external contract.
 */
 interface IACLOracle {
     /**
-    * @dev Tells whether `user` can execute `what`(`how`) in `where` if it's currently set up for `who`
-    * @param user Entity trying to execute `what` in `where`
-    * @param who Entity to which `what` is granted based on the current ACL permissions configuration
-    * @param where Entity where `what` is trying to be executed
-    * @param what Identifier of the action willing to be executed in `where`
-    * @param how Can be used to define a set of arguments to give more context about `what` is trying to be executed in `where`
-    * @return True if the user is allowed to execute the requested action for the given context, false otherwise
+    * @dev Tells whether `sender` can execute `what` (and `how`) in `where` for the grantee `who`
+    * @param who Sender of the original call
+    * @param grantee Grantee of the permission being evaluated
+    * @param where Address of the app
+    * @param what Identifier for a group of actions in app (role)
+    * @param how Permission parameters
+    * @return True if the action should be accepted
     */
-    function canPerform(address user, address who, address where, bytes32 what, uint256[] how) external view returns (bool);
+    function canPerform(address who, address grantee, address where, bytes32 what, uint256[] how) external view returns (bool);
 }
