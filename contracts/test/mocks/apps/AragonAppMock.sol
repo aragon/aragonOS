a6281435b346649817d3bf382cbdfd8be6d82091
ßingen <bingen@aragon.one>
2020-07-27 18:29:28 +0200

    IAragonApp interface: add tests for AragonApp

--- contracts/test/mocks/apps/AragonAppMock.sol
+++ contracts/test/mocks/apps/AragonAppMock.sol
@@ -6,6 +6,10 @@ import "../../../apps/AragonApp.sol";
 contract AragonAppMock is AragonApp {
     bytes4 public constant ARAGON_APP_INTERFACE = ARAGON_APP_INTERFACE_ID;
 
+    function initialize() external {
+        initialized();
+    }
+
     function interfaceID() external pure returns (bytes4) {
         IAragonApp iAragonApp;
         return iAragonApp.kernel.selector ^
