1f50b94ba1b028d77d533d910bc89ccc0064e7e8
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-18 15:00:03 -0300

    agreement: remove activation related functions

--- contracts/apps/disputable/IAgreement.sol
+++ contracts/apps/disputable/IAgreement.sol
@@ -9,9 +9,6 @@ import "../../lib/token/ERC20.sol";
 
 contract IAgreement {
 
-    event DisputableAppActivated(address indexed disputable);
-    event DisputableAppDeactivated(address indexed disputable);
-    event CollateralRequirementChanged(address indexed disputable, uint256 collateralRequirementId);
     event ActionSubmitted(uint256 indexed actionId, address indexed disputable);
     event ActionClosed(uint256 indexed actionId);
     event ActionChallenged(uint256 indexed actionId, uint256 indexed challengeId);
@@ -30,17 +27,6 @@ contract IAgreement {
         Voided
     }
 
-    function activate(
-        address _disputable,
-        ERC20 _collateralToken,
-        uint256 _actionAmount,
-        uint256 _challengeAmount,
-        uint64 _challengeDuration
-    )
-        external;
-
-    function deactivate(address _disputable) external;
-
     function newAction(uint256 _disputableActionId, bytes _context, address _submitter) external returns (uint256);
 
     function closeAction(uint256 _actionId) external;
@@ -50,14 +36,4 @@ contract IAgreement {
     function settleAction(uint256 _actionId) external;
 
     function disputeAction(uint256 _actionId, bool _finishedSubmittingEvidence) external;
-
-    function getDisputableInfo(address _disputable) external view returns (bool activated, uint256 currentCollateralRequirementId);
-
-    function getCollateralRequirement(address _disputable, uint256 _collateralId) external view
-        returns (
-            ERC20 collateralToken,
-            uint256 actionAmount,
-            uint256 challengeAmount,
-            uint64 challengeDuration
-        );
 }
--- contracts/test/mocks/apps/disputable/AgreementMock.sol
+++ contracts/test/mocks/apps/disputable/AgreementMock.sol
@@ -4,15 +4,6 @@ import "../../../../apps/disputable/IAgreement.sol";
 
 
 contract AgreementMock is IAgreement {
-
-    function activate(address, ERC20, uint256, uint256, uint64) external {
-        // do nothing
-    }
-
-    function deactivate(address) external {
-        // do nothing
-    }
-
     function newAction(uint256, bytes, address) external returns (uint256) {
         return 0;
     }
@@ -31,13 +22,5 @@ contract AgreementMock is IAgreement {
 
     function disputeAction(uint256, bool) external {
         // do nothing
-    }
-
-    function getDisputableInfo(address) external view returns (bool, uint256) {
-        return (false, 0);
-    }
-
-    function getCollateralRequirement(address, uint256) external view returns (ERC20, uint256, uint256, uint64) {
-        return (ERC20(0), 0, 0, 0);
     }
 }
